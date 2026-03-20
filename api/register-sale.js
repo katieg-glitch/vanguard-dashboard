18b873420499cedcb19ab5e9e74f7425ae28bf70
katieg-glitch <katieg@pacelink.com>
2026-03-20 09:02:28 -0400

    Update register-sale.js

--- api/register-sale.js
+++ api/register-sale.js
@@ -55,7 +55,6 @@ export default async function handler(req, res) {
     )
 
     const data = await response.json()
-
     return res.status(response.status).json(data)
   } catch (err) {
     return res.status(500).json({
